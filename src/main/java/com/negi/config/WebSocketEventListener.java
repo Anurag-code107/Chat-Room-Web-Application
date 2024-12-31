2a1966f93fa099aebdec4b5e7737e2d7f337e5cb
Anurag <anuragnegi2704@gmail.com>
2024-12-31 08:22:18 +0530

    removed unnecessary codes lines

--- src/main/java/com/negi/config/WebSocketEventListener.java
+++ src/main/java/com/negi/config/WebSocketEventListener.java
@@ -2,9 +2,6 @@ package com.negi.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
